2f5d447cd67b0f78098358ca1401f7f2350a3c42
mememeli <crackofdawn#@163.com>
2022-08-24 22:12:51 +0800

    gaijinban

---
+++ 2022_8_24（改进版）/2022_8_24.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int d = 0;
+	int he = 0;
+	scanf("%d %d", &a, &d);
+	he = a + d;
+	printf("he=%d\n", he);
+	return 0;
+}
